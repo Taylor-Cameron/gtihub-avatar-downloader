b01cc0f61f7fba3fda779fe6d0ee3cf5ce3da398
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-10-30 00:36:37 +0000

    require passcode file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
 var request = require('request');
+var passCode = require('./secrets.js');
 
 console.log('Welcome to the Github Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request';
+    }
+  };
+
+  request(url, function(err, res, body) {
+    callback(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function (err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
